736a8eb07db4745159c3958eeb35435481609710
Alyn.Fosado <210764@utxicotepec.edu.mx>
2024-07-05 12:57:09 -0600

    nuevos cambios

--- BACKEND/app.py
+++ BACKEND/app.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from routes.persona import persona
 from routes.user import user
 app = FastAPI()  # variable app
-app.include_router(persona)  # Luego agrega las rutas
+app.include_router(persons)  # Luego agrega las rutas
 app.include_router(user)
 
 print("Bienvenido al servidor uvicorn")
