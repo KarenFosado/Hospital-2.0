7418f21563a0dcd3bd77da52add14bef5c7d599b
Karen <210764@utxicotepec.edu.mx>
2024-08-07 21:27:36 -0600

    TABLA tbc_organos TERMINADA

--- BACKEND/models/tbc_organos.py
+++ BACKEND/models/tbc_organos.py
@@ -1,13 +1,15 @@
-from sqlalchemy import Column, Integer, String, Boolean, DateTime
-from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, Enum, Text
 from config.db import Base
 
 class Organo(Base):
     __tablename__ = "tbc_organos"
 
     ID = Column(Integer, primary_key=True, index=True)
-    Nombre = Column(String(60))
-    Descripcion = Column(String(200))
-    Estatus = Column(Boolean)
-    Fecha_Registro = Column(DateTime)
-    Fecha_Actualizacion = Column(DateTime)
+    Nombre = Column(String(45), nullable=False)
+    Aparato_Sistema = Column(Enum('Circulatorio', 'Digestivo', 'Respiratorio', 'Nervioso', 'Muscular', 'Esquelético', 'Endocrino', 'Linfático', 'Inmunológico', 'Reproductor', 'Urinario', 'Sensorial'), nullable=False)
+    Descripcion = Column(Text, nullable=False)
+    Disponibilidad = Column(Enum('Disponible', 'No Disponible', 'Reservado'), nullable=False)
+    Tipo = Column(Enum('Vital', 'No Vital'), nullable=False)
+    Fecha_Actualizacion = Column(DateTime, nullable=True)
+    Fecha_Registro = Column(DateTime, nullable=True)
+    Estatus = Column(Boolean, default=True)
