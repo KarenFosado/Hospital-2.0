6e78a3e1a0f8a4745a202bb401ca7376c2cc20ae
Alyn.Fosado <210764@utxicotepec.edu.mx>
2024-08-09 14:05:58 -0600

    NUEVOS CAMBIOS

--- BACKEND/app.py
+++ BACKEND/app.py
@@ -4,12 +4,27 @@ from routes.person import person
 from routes.rol import rol
 from routes.userrol import userrol
 from routes.tbc_organos import tbc_organos  # Importar el enrutador de organos
+from fastapi.middleware.cors import CORSMiddleware
+
+
 
 app = FastAPI()  # variable app
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 app.include_router(person)  # Luego agrega las rutas
 app.include_router(user)
 app.include_router(rol)
 app.include_router(userrol)
 app.include_router(tbc_organos)  # Agregar el enrutador de organos
+
+
+
+app
 
 print("Bienvenido al servidor uvicorn")
--- FRONTEND/sistema-hospital/src/components/OrganForm.vue
+++ FRONTEND/sistema-hospital/src/components/OrganForm.vue
@@ -1,24 +1,43 @@
 <template>
     <form>
-        <div class="mx-auto max-w-5xl px-4 sm:px-6 lg:px-8 rounded-lg shadow-lg overflow-hidden">
+        <div class="mx-auto max-w-4xl px-4 sm:px-6 lg:px-8 rounded-lg shadow-lg overflow-hidden mt-5">
             <!-- Done Information -->
-            <div class="flex justify-between flex flex-col items-center">
-                <label class="block text-gray-700 text-3xl font-normal mb-2">Informacion del Donatario</label>
-            </div>
             
+
+            <div class="flex justify-between  flex-col items-center">
+                <label class="bloc k text-gray-700 text-3xl font-normal mb-2">Informacion del Donatario</label>
+            </div>
            
-           
-           
+        
             <div class="mt-4">
                 <div class="flex justify-between">
-                    <label class="block text-gray-700 text-sm font-bold mb-2">Organo</label>
+                    <label class="block text-gray-700 text-sm font-bold mb-2">Nombre</label>
+                </div>
+                <input
+                    class=" bg-gray-200 text-gray-700 focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block w-full appearance-none"
+                    type="text" disabled />
+            </div>
+            
+            <div class="mt-4">
+                <div class="flex justify-between">
+                    <label class="block text-gray-700 text-sm font-bold mb-2">Aparato y Sistema</label>
                 </div>
                 <select
                     className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline text-center">
                     <option value="">-- Seleciona un Organo --</option>
                 </select>
             </div>
-            
+
+            <div class="mt-4">
+                <div class="flex justify-between">
+                    <label class="block text-gray-700 text-sm font-bold mb-2">Nombre del Donatario</label>
+                </div>
+                <input
+                    class="bg-gray-200 text-gray-700 focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block w-full appearance-none"
+                    type="text" disabled />
+            </div>
+
+
             <div class="mt-4">
                 <div class="flex justify-between">
                     <label class="block text-gray-700 text-sm font-bold mb-2">Fecha de Registro</label>
@@ -39,22 +58,8 @@
             <div class="flex justify-between flex flex-col items-center mt-5">
                 <label class="block text-gray-700 text-3xl font-normal mb-2">Resumen de la Donacion</label>
             </div>
-            <div class="mt-4">
-                <div class="flex justify-between">
-                    <label class="block text-gray-700 text-sm font-bold mb-2">Nombre del Donador</label>
-                </div>
-                <input
-                    class=" bg-gray-200 text-gray-700 focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block w-full appearance-none"
-                    type="text" disabled />
-            </div>
-            <div class="mt-4">
-                <div class="flex justify-between">
-                    <label class="block text-gray-700 text-sm font-bold mb-2">Nombre del Donatario</label>
-                </div>
-                <input
-                    class="bg-gray-200 text-gray-700 focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block w-full appearance-none"
-                    type="text" disabled />
-            </div>
+          
+            
             <div class="mt-4">
                 <div class="flex justify-between">
                     <label class="block text-gray-700 text-sm font-bold mb-2">Nombre del Medico</label>
